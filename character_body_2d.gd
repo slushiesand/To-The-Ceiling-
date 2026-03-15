8835d311889dd767367f513bc5ec067c232db5a5
slushie/ampersand <slushiesand@gmail.com>
2026-03-15 17:22:21 -0400

    art added, coyote jump

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,26 +1,37 @@
-extends CharacterBody2D
+class_name Player extends CharacterBody2D
 
+var coyote_frames = 2
+var is_coyote = false
+#obviously, you're a ceiling. but you can be a coyote as a treat. same kingdom anyway.
+var last_floor = false
+var jumping = false
 
-const SPEED = 250
-const JUMP_VELOCITY = -900.0
+const SPEED = 200
+const JUMP_VELOCITY = -700.0
 
+func _ready() -> void:
+	$Timer.wait_time = coyote_frames / 60.0
 
 func _physics_process(delta: float) -> void:
-	var tween = get_tree().create_tween().set_ease(Tween.EASE_IN)
+	last_floor = is_on_floor()
 
 	if not is_on_floor():
-		tween.pause()
 		velocity += get_gravity() * delta
 	else:
-		#scale = Vector2(1, 0.7)
-		tween.tween_property($Sprite2D, "scale:y", 1.0, 0.5)
-		
-	if Input.is_action_just_pressed("up") and is_on_floor():
+		jumping = false
+	
+	#https://kidscancode.org/godot_recipes/4.x/2d/coyote_time/index.html
+	
+	if Input.is_action_just_pressed("up") and (is_on_floor() or is_coyote):
 		velocity.y = JUMP_VELOCITY
+		jumping = true
+	
+	if not is_on_floor() and not jumping:
+		is_coyote = true
+		$Timer.start()
 
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		tween.pause()
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
@@ -29,7 +40,8 @@ func _physics_process(delta: float) -> void:
 	elif Input.is_action_pressed("right"):
 		$Sprite2D.set_flip_h(false)
 		
-	
-		
-
 	move_and_slide()
+
+func _on_timer_timeout() -> void:
+	is_coyote = false
+	#you are now a ceiling.
--- label.gd
+++ label.gd
@@ -1,16 +1,5 @@
 extends Label
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	set_text("yes")
+	if body is Player:
+		set_text("yes")
