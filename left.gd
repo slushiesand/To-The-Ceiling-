d43967135da36a5745112d677cb469951831bc5f
slushie/ampersand <slushiesand@gmail.com>
2026-03-16 00:17:44 -0400

    i forgot to delete 2 files mb

--- left.gd
+++
@@ -1,8 +0,0 @@
-extends ColorRect
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body is Player:
-		await get_tree().create_timer(9.0).timeout
-		$".".set_visible(true)
-		var tween = get_tree().create_tween()
-		tween.tween_property($".", "position:x", -172.0, 4.0)
--- right.gd
+++
@@ -1,8 +0,0 @@
-extends ColorRect
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body is Player:
-		await get_tree().create_timer(9.0).timeout
-		var tween = get_tree().create_tween()
-		$".".set_visible(true)
-		tween.tween_property($".", "position:x", 60.0, 4.0)
